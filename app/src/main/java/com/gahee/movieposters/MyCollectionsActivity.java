bb83848e8af6d8d8377b41836dafedccc82c691b
Gahee Han <leisure306@gmail.com>
2019-08-27 21:20:32 +0900

    Make layout for my collections activity

---
+++ app/src/main/java/com/gahee/movieposters/MyCollectionsActivity.java
@@ -0,0 +1,14 @@
+package com.gahee.movieposters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyCollectionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_collections);
+    }
+}
